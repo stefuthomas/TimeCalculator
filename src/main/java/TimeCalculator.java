a04645ea2a9d3c1cf0c9f3fe1b878b6853aa8e74
Stefanos Thomas <stefanos.thomas@metropolia.fi>
2024-09-23 15:02:49 +0300

    Dockerfile added

--- src/main/java/TimeCalculator.java
+++ src/main/java/TimeCalculator.java
@@ -1,4 +1,11 @@
 public class TimeCalculator {
+    public static void main(String[] args) {
+        TimeCalculator timeCalculator = new TimeCalculator();
+        double distance = 100;
+        double speed = 10;
+        double time = timeCalculator.calculateTime(distance, speed);
+        System.out.println("Time taken to travel " + distance + " km at " + speed + " km/h is " + time + " hours.");
+    }
     public double calculateTime(double distance, double speed) {
         if (speed <= 0) {
             throw new IllegalArgumentException("Speed must be greater than zero.");
